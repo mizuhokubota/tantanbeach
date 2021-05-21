b87e89718c5668e28a75da465bdde56196be72b3
久保田瑞穂 <>
2021-05-21 19:36:04 +0900

    更新しました

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,16 @@
+
+
 $(document).on('ready', function() {
-    $(".full-screen-o").slick({
-      centerMode: true, // スライドを中心に表示して部分的に前後のスライドが見えるように設定（奇数番号のスライドに使用）
-      centerPadding: '5%', // センターモード時のサイドパディング。見切れるスライドの幅。’px’または’％’。
-      dots: true, // ドットインジケーターの表示
-      autoplay: true, // 自動再生を設定
-      autoplaySpeed: 3000, // 自動再生のスピード（ミリ秒単位）
-      speed: 1000, // スライド/フェードアニメーションの速度を設定
-      infinite: true // スライドのループを有効にするか
-    });
+  $(".full-screen").slick({
+    centerMode: true, // スライドを中心に表示して部分的に前後のスライドが見えるように設定（奇数番号のスライドに使用）
+    centerPadding: '5%', // センターモード時のサイドパディング。見切れるスライドの幅。’px’または’％’。
+    dots: true, // ドットインジケーターの表示
+    autoplay: true, // 自動再生を設定
+    autoplaySpeed: 3000, // 自動再生のスピード（ミリ秒単位）
+    speed: 1000, // スライド/フェードアニメーションの速度を設定
+    infinite: true // スライドのループを有効にするか
   });
+
+});
+
+console.log($(".full-screen"));
