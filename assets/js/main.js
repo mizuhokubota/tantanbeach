4fc72937494148ef267aa561b081635df942ea1c
久保田瑞穂 <>
2021-05-22 12:33:36 +0900

    ・６ビーチの画像を全て縦横200pxのpngにして保存しました。 ・６ビーチのhtmlのベースを作り、６画像とリンクしました。（説明部分を例のブログから丸パクリしてます。下里さんよろしければ少しアレンジしてもらえますか？ばば海岸はブログに載ってなかったのでそのまま空白です。） ・６画像が横並びにはなりましたがslickがうまくできていません。（時間の無駄だと思って諦めました笑。画像の材料は揃ってるので先生に聞けば多分すぐできると思います笑。）

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,57 @@
 $(function(){
 
-    $(document).on('ready', function() {
-        $(".full-screen").slick({
-          centerMode: true,
-          centerPadding: '5%',
-          dots: true,
-          autoplay: true,
-          autoplaySpeed: 3000,
-          speed: 1000,
-          infinite: true,
-        });
-      });
+  console.log($(".full-screen"));
+  
+  // section1のスライダー用
+  　$('.bgimg-slider').bgSwitcher({
+    　　images: ['../img/topview1.JPG','../img/topview2.JPG','../img/topview3.JPG'],
+    　　effect: "drop",
+    　　easing: "linear"
+    　});
+  
+  
+  
 
-})
-
-
-
-$(document).on('ready', function() {
-  $(".full-screen").slick({
-    centerMode: true, // スライドを中心に表示して部分的に前後のスライドが見えるように設定（奇数番号のスライドに使用）
-    centerPadding: '5%', // センターモード時のサイドパディング。見切れるスライドの幅。’px’または’％’。
-    dots: true, // ドットインジケーターの表示
-    autoplay: true, // 自動再生を設定
-    autoplaySpeed: 3000, // 自動再生のスピード（ミリ秒単位）
-    speed: 1000, // スライド/フェードアニメーションの速度を設定
-    infinite: true // スライドのループを有効にするか
+    
+  
+  
+  // section2
+  $(function() {
+    $('.beaches').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 6,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+            
   });
-
-});
-
-console.log($(".full-screen"));
-
-// section1のスライダー用
-　$('.bgimg-slider').bgSwitcher({
-  　　images: ['../img/topview1.JPG','../img/topview2.JPG','../img/topview3.JPG'],
-  　　effect: "drop",
-  　　easing: "linear"
-  　});
-
+  });
